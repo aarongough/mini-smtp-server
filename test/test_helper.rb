847ded91d32b704a4114ff4425d12cd1eec9b949
aarongough <aaron@aarongough.com>
2010-11-04 16:41:11 -0400

    Updated test helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,5 @@
-TEST_LIVE_API = false
-
 require 'rubygems'
 require 'test/unit'
-
-unless(TEST_LIVE_API)
-  require 'webmock/test_unit'
-  include WebMock
-end
 
 require_files = []
 require_files << File.join(File.dirname(__FILE__), '..', 'lib', 'ruby-tmdb.rb')
@@ -15,6 +8,3 @@ require_files.concat Dir[File.join(File.dirname(__FILE__), 'setup', '*.rb')]
 require_files.each do |file|
   require File.expand_path(file)
 end
-
-#load(File.join('unit', 'test_direct_require.rb'), true)
-system('ruby ' + File.expand_path(File.join(File.dirname(__FILE__), 'unit', 'test_direct_require.rb')))
